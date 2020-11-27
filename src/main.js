8d6b917c5538ffa2ef7e12e12c897c67623082f8
Marcelino Meza <kmeza@ucol.mx>
2020-11-27 15:19:24 -0600

    added retirar function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 export default class App {
 
+    // Función que
+    retirar(saldo, retiro){
+
+        let restante = saldo - retiro
+
+        if(retiro > saldo){
+            restante = -1;
+        }else{
+            return restante
+        }
+
+        return restante;
+
+    }
+
+    // Función que
+
+
+    // Función que
+
 }
 
 let app = new App();
+
+// Función 1
+console.log(app.retirar(100, 80));
+console.log(app.retirar(110, 200));
+console.log(app.retirar(90, 90));
