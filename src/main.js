648794eb88ceb5f4abfdf0ad49409619f957230d
Marcelino Meza <kmeza@ucol.mx>
2020-11-27 15:46:20 -0600

    added totalVenta function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 export default class App {
 
-    // Función que
+    // Función que retire dinero de una cuenta
     retirar(saldo, retiro){
 
         let restante = saldo - retiro
@@ -8,15 +8,31 @@ export default class App {
         if(retiro > saldo){
             restante = -1;
         }else{
-            return restante
+            return restante;
         }
 
         return restante;
 
     }
 
-    // Función que
+    // Función que calcule el descuento de una venta
 
+    totalVenta(costoProducto, numeroProductos){
+
+        let total = costoProducto * numeroProductos;
+        let descuento
+
+        if(numeroProductos < 10){
+            descuento = total * 0.95;
+        }else if(numeroProductos >= 10 && numeroProductos <= 20){
+            descuento = total * 0.9;
+        }else if(numeroProductos > 20){
+            descuento = total * 0.85;
+        }
+
+        return descuento;
+
+    }
 
     // Función que
 
@@ -28,3 +44,9 @@ let app = new App();
 console.log(app.retirar(100, 80));
 console.log(app.retirar(110, 200));
 console.log(app.retirar(90, 90));
+
+// Función 2
+console.log(app.totalVenta(10, 5));
+console.log(app.totalVenta(10, 10));
+console.log(app.totalVenta(10, 20));
+console.log(app.totalVenta(10, 30));
